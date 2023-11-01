4c01476e14cdc830759910067151aa81f87e58b5
Vishal <vishal10kesharwani@gmail.com>
2023-11-01 16:33:18 +0530

    new

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -11,16 +11,17 @@ import {
   Button,
 } from "@nextui-org/react";
 import { MoonIcon } from "../components/MoonIcon";
+import Logo1 from "../assets/logo.png";
 import { SunIcon } from "../components/SunIcon";
 
 export default function App() {
   const { theme, setTheme } = useTheme();
   return (
     <Navbar isBordered className="">
+      <NavbarBrand className="mr-4">
+        <img src={Logo1} className="w-20" />
+      </NavbarBrand>
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        <NavbarBrand className="mr-4">
-          <p className="hidden sm:block font-bold text-inherit">ARcane</p>
-        </NavbarBrand>
         <NavbarItem>
           <Link color="foreground" href="#">
             Buy
