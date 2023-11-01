0353aa2fb10b03fc79f53383069bfaf49a516c10
Adarsh Jena <76236137+adarshnjena@users.noreply.github.com>
2023-11-01 17:45:33 +0530

    fiter list added

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -149,76 +149,85 @@ const Home = () => {
     <Layout>
       {isLoading ? <Loader width="500px" height="250px" /> : null}
       <Filter>
-        <Card
-          isBlurred
-          className="border-none bg-background/60 dark:bg-default-100/50 w-[100%] "
-          shadow="sm"
-        >
-          <CardBody>
-            <div className="grid items-center justify-center grid-cols-6 gap-6 md:grid-cols-12 md:gap-4">
-              <div className="relative col-span-6 md:col-span-4">
-                <Image
-                  alt="Album cover"
-                  className="object-cover"
-                  height={200}
-                  shadow="md"
-                  // src=""
-                  src={propertyList?.fetchProp[14]?.image}
-                  width="100%"
-                />
-              </div>
-
-              <div className="flex flex-col col-span-6 md:col-span-8">
-                <div className="flex items-start justify-between">
-                  <div className="flex flex-col gap-0">
-                    <h3 className="font-semibold text-foreground/90">
-                      {console.log(propertyList)}
-                      {propertyList?.fetchProp[14]?.title}
-                    </h3>
-                    <p className="text-small text-foreground/80">12 Tracks</p>
-                    <h1 className="mt-2 font-medium text-large">
-                      Frontend Radio
-                    </h1>
-                  </div>
-                  <Button
-                    isIconOnly
-                    className="text-default-900/60 data-[hover]:bg-foreground/10 -translate-y-2 translate-x-2"
-                    radius="full"
-                    variant="light"
-                    onPress={() => setLiked((v) => !v)}
-                  >
-                    <HeartIcon
-                      className={liked ? "[&>path]:stroke-transparent" : ""}
-                      fill={liked ? "currentColor" : "none"}
-                    />
-                  </Button>
-                </div>
-
-                <div className="flex flex-col gap-1 mt-3">
-                  <div className="flex justify-between">
-                    <p className="text-small">1:23</p>
-                    <p className="text-small text-foreground/50">4:32</p>
-                  </div>
-                  <Web3Button
-                    contractAddress={import.meta.env.VITE_CONTRACT_ADDRESS}
-                    action={(contract) => {
-                      contract.call("createTransaction", [
-                        address,
-                        12,
-                        3000000000000000,
-                      ]);
-                    }}
-                  >
-                    Buy Now
-                  </Web3Button>
-                </div>
-              </div>
+        {[1, 2, 3, 4].map((item, index) => {
+          return (
+            <div key={index} className=" mx-2 mb-10">
+              {Cards(propertyList, setLiked, liked, address)}
             </div>
-          </CardBody>
-        </Card>
+          );
+        })}
       </Filter>
     </Layout>
   );
 };
 
 export default Home;
+function Cards(propertyList, setLiked, liked, address) {
+  return (
+    <Card
+      isBlurred
+      className="border-none bg-background/60 dark:bg-default-100/50 w-[100%] h-[35vh] "
+      shadow="sm"
+    >
+      <CardBody>
+        <div className="grid items-center justify-center grid-cols-6 gap-6 md:grid-cols-12 md:gap-4">
+          <div className="relative col-span-6 md:col-span-4">
+            <Image
+              alt="Album cover"
+              className="object-cover"
+              height={200}
+              shadow="md"
+              // src=""
+              src={propertyList?.fetchProp[14]?.image}
+              width="100%"
+            />
+          </div>
+
+          <div className="flex flex-col col-span-6 md:col-span-8">
+            <div className="flex items-start justify-between">
+              <div className="flex flex-col gap-0">
+                <h3 className="font-semibold text-foreground/90">
+                  {console.log(propertyList)}
+                  {propertyList?.fetchProp[14]?.title}
+                </h3>
+                <p className="text-small text-foreground/80">12 Tracks</p>
+                <h1 className="mt-2 font-medium text-large">Frontend Radio</h1>
+              </div>
+              <Button
+                isIconOnly
+                className="text-default-900/60 data-[hover]:bg-foreground/10 -translate-y-2 translate-x-2"
+                radius="full"
+                variant="light"
+                onPress={() => setLiked((v) => !v)}
+              >
+                <HeartIcon
+                  className={liked ? "[&>path]:stroke-transparent" : ""}
+                  fill={liked ? "currentColor" : "none"}
+                />
+              </Button>
+            </div>
+
+            <div className="flex flex-col gap-1 mt-3">
+              <div className="flex justify-between">
+                <p className="text-small">1:23</p>
+                <p className="text-small text-foreground/50">4:32</p>
+              </div>
+              <Web3Button
+                contractAddress={import.meta.env.VITE_CONTRACT_ADDRESS}
+                action={(contract) => {
+                  contract.call("createTransaction", [
+                    address,
+                    12,
+                    3000000000000000,
+                  ]);
+                }}
+              >
+                Buy Now
+              </Web3Button>
+            </div>
+          </div>
+        </div>
+      </CardBody>
+    </Card>
+  );
+}
