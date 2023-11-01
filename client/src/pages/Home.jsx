49e371aa806b77aa77d1baebc1fb9d644a3c3459
Siddhant Patil <shimpli.patil2804@gmail.com>
2023-11-01 16:46:43 +0530

    staging

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -12,50 +12,8 @@ import { ethers } from "ethers";
 import { HeartIcon } from "../components/HeartIcon";
 
 const Home = () => {
-  const { theme, setTheme } = useTheme();
-  const navigateTo = useNavigate();
-  const { user, setUser, propertyList, setPropertyList } = useGlobalContext();
   const [isLoading, setIsLoading] = useState(false);
   const [liked, setLiked] = useState(false);
-
-  const getPropertyList = async () => {
-    setIsLoading(true);
-
-    try {
-      const res = await axios.get("/api/v1/properties/fetch");
-      setPropertyList(res.data);
-      setIsLoading(false);
-    } catch (err) {
-      console.error(err);
-      setIsLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    getPropertyList();
-    console.log("useEffect is running");
-    console.log(propertyList);
-  }, []);
-
-  const logout = () => {
-    localStorage.removeItem("user");
-    navigateTo("/");
-    setUser(null);
-  };
-
-  const deleteUser = async (e) => {
-    e.preventDefault();
-    setIsLoading(true);
-
-    try {
-      const res = await axios.post("/user/delete");
-      setIsLoading(false);
-      logout();
-    } catch (error) {
-      setIsLoading(false);
-      console.log(error);
-    }
-  };
 
   const address = useAddress();
   const { contract } = useContract(import.meta.env.VITE_CONTRACT_ADDRESS);
@@ -74,98 +32,21 @@ const Home = () => {
     <>
       <Navbar />
       {isLoading ? <Loader width="500px" height="250px" /> : null}
-      <div className="flex justify-between m-2">
-        <div className="m-2 text-2xl text-center">Home</div>
-        <User
-          name={user.result.name}
-          description={user.result.email}
-          avatarProps={{
-            src:
-              (user && user.result.picture) ||
-              "https://img.icons8.com/?size=256&id=kDoeg22e5jUY&format=png",
-          }}
-        />
-        <div className="flex justify-between">
-          <Switch
-            defaultSelected
-            size="lg"
-            color="primary"
-            thumbIcon={({ isSelected, className }) =>
-              !isSelected ? (
-                <SunIcon className={className} />
-              ) : (
-                <MoonIcon className={className} />
-              )
-            }
-            onClick={() => {
-              if (theme === "light") {
-                setTheme("dark");
-              } else if (theme === "dark") {
-                setTheme("light");
-              }
-            }}
-          />
-          <Button
-            onClick={logout}
-            className="flex m-2"
-            color="danger"
-            variant="bordered"
-            startContent={<RiLogoutCircleLine />}
-          >
-            logout
-          </Button>
-          <Button
-            onClick={deleteUser}
-            className="flex m-2"
-            color="danger"
-            variant="shadow"
-            startContent={<MdDeleteOutline />}
-          >
-            Delete Account
-          </Button>
-        </div>
-      </div>
       <Card
-        isFooterBlurred
-        className="w-[300px] h-[300px] col-span-12 sm:col-span-5"
+        isBlurred
+        className="border-none bg-background/60 dark:bg-default-100/50 max-w-[610px]"
+        shadow="sm"
       >
-        <CardHeader className="absolute z-10 flex-col items-start top-1">
-          <p className="font-bold uppercase text-tiny text-black/60">2BHK</p>
-          <h4 className="text-2xl font-medium text-gray">Nagpur</h4>
-        </CardHeader>
-
-        <Image
-          removeWrapper
-          alt="Card example background"
-          className="z-0 object-cover w-full h-full scale-125 -translate-y-6"
-          src="https://images.unsplash.com/photo-1564078516393-cf04bd966897?auto=format&fit=crop&q=80&w=1974&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        />
-        <CardFooter className="absolute bottom-0 z-10 justify-between bg-white/30 border-t-1 border-zinc-100/50">
-          <div>
-            <p className="text-black text-tiny">Available.</p>
-            <p className="text-black text-tiny">Get notified.</p>
-          </div>
-          <Button
-            onClick={() => navigateTo("/details")}
-            className="text-tiny"
-            color="primary"
-            radius="full"
-            size="sm"
-            disabled
-          >
-            Visit Home
-          </Button>
-        </CardFooter>
-      </Card>
-      <main className="main">
-        <div className="container">
-          <div className="header">
-            <div className="connect">
-              <ConnectWallet
-                dropdownPosition={{
-                  side: "bottom",
-                  align: "center",
-                }}
+        <CardBody>
+          <div className="grid items-center justify-center grid-cols-6 gap-6 md:grid-cols-12 md:gap-4">
+            <div className="relative col-span-6 md:col-span-4">
+              <Image
+                alt="Album cover"
+                className="object-cover"
+                height={200}
+                shadow="md"
+                src="https://images.unsplash.com/photo-1564078516393-cf04bd966897?auto=format&fit=crop&q=80&w=1974&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                width="100%"
               />
             </div>
 
