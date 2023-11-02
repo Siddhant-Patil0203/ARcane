79e7f8449ee7dfa613cae1697f47e272fad1bfc4
Vishal <vishal10kesharwani@gmail.com>
2023-11-02 12:45:41 +0530

    vv

--- server/models/properties.js
+++ server/models/properties.js
@@ -2,40 +2,52 @@ import mongoose from "mongoose";
 
 // yet to add required
 const propertySchema = new mongoose.Schema({
-    title:{
-        type:String,
-    },
-    image:{
-        type:String,
-    },
-    description:{
-        type:String,
-    },
-    price:{
-        type:Number
-    },
-    location:{
-        type:String
-    },
-    size:{
-        type:String
-    },
-    status:{
-        type:String,
-        default:"Listed",
-    },
-    user:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:"Users",
-        required:true,
-    },
-    blockchainId:{
-        type:String,
-    },
-    ListingDate:{
-        type : Date,
-        default : Date.now,
-    }
-})
+  title: {
+    type: String,
+  },
+  image: {
+    type: String,
+  },
+  description: {
+    type: String,
+  },
+  price: {
+    type: Number,
+  },
+  location: {
+    type: String,
+  },
+  size: {
+    type: String,
+  },
+  status: {
+    type: String,
+    default: "Listed",
+  },
+  link1: {
+    type: String,
+  },
+  link2: {
+    type: String,
+  },
+  link3: {
+    type: String,
+  },
+  link4: {
+    type: String,
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Users",
+    required: true,
+  },
+  blockchainId: {
+    type: String,
+  },
+  ListingDate: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-export default mongoose.model("Properties",propertySchema);
+export default mongoose.model("Properties", propertySchema);
